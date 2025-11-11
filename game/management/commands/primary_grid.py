735ffc5f7c67e71cc90260043341e1ea828c129e
colin-ingraham <colinringraham@gmail.com>
2025-11-11 16:45:53 -0500

    Intro to CIA Algorithm, LAST COMMIT BEFORE NEW MODELS

--- game/management/commands/primary_grid.py
+++ game/management/commands/primary_grid.py
@@ -1,3 +1,8 @@
+# Primary Grid is LEGACY
+# All the code below is no longer the algorithm used in Litgrid
+# For the modern algorithm, referencing the Combinatorial Interesection Algorithm, otherwise referenced as CIA.py
+
+
 """" 
 This is the algorithm for Primary Grid, the main Litgrid category generator 
 that uses specific categories and an algorithm to generate daily Litgrids. 
@@ -43,15 +48,28 @@ from django.core.management.base import BaseCommand
 from library.models import Book, Author
 import random
 
-AUTHOR_CAT_CHOICES = ["author", "nationality", "first_name"]
+AUTHOR_CAT_CHOICES = ["author", "nationality", "first_name", "author", "first_name"]
+PRIMARY_SUBJECTS = ["science fiction", "historical fiction", "fantasy", "novela", 
+                    "classic literature", "american literature", "classics", "african americans", 
+                    "politics and government", "horror", "humor", "adventure", "thriller", "nonfiction", 
+                    "fiction, coming of age", "romance", "magic", "drama", "detective and mystery stories", 
+                    "adventure", "lawyers", "psychological fiction", "love", "murder", "farm life", "children's fiction"]
+
 class Command(BaseCommand):
 
        def handle(self, *args, **options):
-     
-              # 1. Get an author category
-              self.choose_author_cat()
+              
+              # The categories are selected in the following process
 
-              # 2. Choose Misc category, 
+              # 1. Get an author category
+              author_cat, author = self.choose_author_cat()
+
+              # 2. Choose Misc, GenreV and Subject based on Author
+              subject = self.choose_subject(author_cat, author)
+              self.test_author_subject_combo(author_cat, author, subject)
+              # 3. Choose GenreH and Time Period based on column categories
+
+              # 4. Verify playability
               
 
        
@@ -61,11 +79,12 @@ class Command(BaseCommand):
                      author = self.get_author_with_first_name()
                      self.stdout.write(self.style.SUCCESS(f"Author category: Author with first name {author}"))
               elif (author_cat == "nationality"):
-                     country = self.get_acceptable_country()
-                     self.stdout.write(self.style.SUCCESS(f"Author category: Authors from {country}"))
+                     author = self.get_acceptable_country()
+                     self.stdout.write(self.style.SUCCESS(f"Author category: Authors from {author}"))
               else: # Specific Author
                      author = self.get_prominent_author()
                      self.stdout.write(self.style.SUCCESS(f"Author category: Books by {author}"))
+              return author_cat, author
 
        def get_author_with_first_name(self):
               authors = self.common_first_names()
@@ -151,4 +170,89 @@ class Command(BaseCommand):
                             common_names.append(name)
 
               return common_names
-       
+
+       def get_authors_from_cat(self, author_cat, author):
+              # This method is going to return the authors upon which the Author cat can be.
+              authors = []
+              if author_cat == "first_name":
+                     for x in Author.objects.all():
+                            if x.first_name == author:
+                                   authors.append(x.name)
+              elif author_cat == "nationality":
+                     for x in Author.objects.all():
+                            if x.nationality == author:
+                                   authors.append(x.name)          
+              else:
+                     authors.append(author)
+              return authors
+
+       def get_author_prominent_subjects(self, author_cat, author):
+              # This method is going to return a list of the most common subjects from a specific author/authors
+              # One of these subjects will be chosen as the subject used in the grid.
+              
+              min_books = 2
+              possible_authors = self.get_authors_from_cat(author_cat, author)
+              subject_counts = {}
+              prominent_subjects = []
+
+              for a in possible_authors:
+                     books = list(Book.objects.filter(author__name=a))
+                     for book in books:
+                            subjects = book.subjects.all()
+                            for subject in subjects:
+                                   if subject.name in subject_counts:
+                                          subject_counts[subject.name] += 1
+                                   else:
+                                          subject_counts[subject.name] = 1
+              
+              for key, var in subject_counts.items():
+                     if var >= min_books:
+                            prominent_subjects.append(key)
+
+              return prominent_subjects
+
+       def choose_subject(self, author_cat, author):
+              # This method is going to actually choose the subject for the grid.
+              # If the author's prominent subjects contains one of the PRIMARY_SUBJECTS, then it will choose one of those.
+              # Otherwise, it will choose one of the most likely, often under certain restraints. 
+
+              prom_subs = self.get_author_prominent_subjects(author_cat, author)
+              candidates = []
+              for subject in prom_subs:
+                     if subject in PRIMARY_SUBJECTS:
+                            candidates.append(subject)
+              if len(candidates) >= 1:
+                     choice = random.choice(candidates)
+                     self.stdout.write(self.style.SUCCESS(f"Subject category: {choice}"))
+                     return choice
+              else:
+                     self.stdout.write(self.style.SUCCESS(f"No popular subject category found."))
+                     return "No Subject"
+
+       def most_common_subjects(self):
+              # This method finds the most common subjects of all books. 
+              # It is not used in the primary grid, but instead used manually to create the PRIMARY_SUBJECTS list for subject selection
+              subject_min = 5
+              subject_counts = {}
+              books = list(Book.objects.all())
+              for book in books:
+                     subjects = book.subjects.all()
+                     for subject in subjects:
+                            if subject.name in subject_counts:
+                                   subject_counts[subject.name] += 1
+                            else:
+                                   subject_counts[subject.name] = 1
+              sorted_subjects = {k: v for k, v in sorted(subject_counts.items(), key=lambda item: item[1], reverse=True) if v >= subject_min}
+              return sorted_subjects
+
+       def test_author_subject_combo(self, author_cat, author, subject):
+              # Returns the books possible for the given author and subject
+              if subject != "No Subject":
+                     authors = self.get_authors_from_cat(author_cat, author)
+                     books = Book.objects.filter(subjects__name=subject, author__name__in=authors)
+                     self.stdout.write(self.style.SUCCESS(f"Possible books for Cell 3 (Author/Subject): {books}"))
+              
+              
+
+
+              
